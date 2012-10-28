47edbd261af741f2338083be5d27afc00953088d
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2012-10-28 19:36:24 +0100

    Adding pages

---
+++ javascripts/transhelper.js
@@ -0,0 +1,43 @@
+function output(inp) {
+    document.getElementById("transcription").innerHTML = inp;
+}
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+
+function parse() {
+  var replies = [];
+  var lines = $("#rawtrans").val().split("\n");
+  $.each(lines, function(n, elem) {
+    var i = elem.indexOf(':');
+    if(i != -1) {
+      var replyComponents = [elem.slice(0,i), elem.slice(i+1)];
+      console.log(replyComponents);
+      var reply = {"part" : replyComponents[0], "text" : $.trim(replyComponents[1])};
+      replies.push(reply);
+    }
+  });
+  console.log(replies);
+  var str = JSON.stringify({"dialog" : replies}, undefined, 4);
+  //document.getElementById("transcription").innerHTML = syntaxHighlight(JSON.stringify({"dialog" : replies}));
+  output(syntaxHighlight(str));
+  //$("#transcription").append($.dump(replies));
+}
+
