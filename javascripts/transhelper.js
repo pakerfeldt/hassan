56906810ee4f0bf204f5378c87ad38094d23e6cb
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2012-10-29 19:25:59 +0100

    Replacing copy to clipboard with a select-text button.

--- javascripts/transhelper.js
+++ javascripts/transhelper.js
@@ -52,7 +52,16 @@ function parse() {
   return str;
 }
 
-function copy() {
-   window.prompt ("Copy to clipboard: Ctrl+C/Cmd+C, Enter", parse());
+function selectText(containerid) {
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+    }
 }
+
 
